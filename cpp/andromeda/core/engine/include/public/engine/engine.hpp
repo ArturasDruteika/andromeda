efac6004441638dec2c1bedf6555ef8376e84d75
Artūras Drūteika <arturas.druteika@gmail.com>
2026-09-24 12:45:21 +0300

    Refactors engine into private and public headers

--- cpp/andromeda/core/engine/include/public/engine/engine.hpp
+++ cpp/andromeda/core/engine/include/public/engine/engine.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 
-#include "macro_exports.hpp"
+#include "../private/macro_exports.hpp"
 #include "andromeda/components/graphics_backend.hpp"
 #include "andromeda/engine/i_engine.hpp"
 
